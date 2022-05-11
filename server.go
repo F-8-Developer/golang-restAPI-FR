28744197d55c78a5cd590460d1cd1443621c912a
Ax7-cmd <faishal.amrullah@bitcoin.co.id>
2022-05-12 03:13:34 +0700

    update docker container & update .env

--- server.go
+++ server.go
@@ -23,19 +23,6 @@ func main() {
 	Database.Mysql.AutoMigrate(&Models.User{})
 	Database.Mysql.AutoMigrate(&Models.Friend{})
 
-	// GRPC
-	// Here will enable grpc server, if you don`t want it, you can disable it
-	// go func() {
-	// 	lis, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", 10000))
-	// 	if err != nil {
-	// 		log.Fatalf("failed to listen: %v", err)
-	// 	}
-	// 	var opts []grpc.ServerOption
-	// 	grpcServer := grpc.NewServer(opts...)
-	// 	pb.RegisterRouteGuideServer(grpcServer, mgrpc.NewServer())
-	// 	grpcServer.Serve(lis)
-	// }()
 	app_env := Config.GoDotEnvVariable("APP_ENV")
-
 	Router.Start(app_env)
 }
